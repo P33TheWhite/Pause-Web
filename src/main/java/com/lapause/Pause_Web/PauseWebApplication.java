59f87bbb80e3cc53506281045c778875edc803e7
P33TheWhite <paul33lb@gmail.com>
2025-11-27 21:10:55 +0100

    Initialisation du Projet

---
+++ src/main/java/com/lapause/Pause_Web/PauseWebApplication.java
@@ -0,0 +1,13 @@
+package com.lapause.Pause_Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PauseWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PauseWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lapause/Pause_Web/PauseWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lapause.Pause_Web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PauseWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
